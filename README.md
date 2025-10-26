344c18d785ac5c274ffb9090e864c54d99a981bd
elarle <arbol@git.ak>
2025-10-26 14:26:52 +0100

    Partial snake movement.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Snake Game
 Classic snake game
+
+### How the game works
+The program stores a matrix of numbers where all information is stored.
+
+Number 0 represents an empty space.
+A diferent number can represent two things:
+ * If it is between 1 and 127 (this will change in the future), the snake ocupies that position.
+ * If its not an snake position, its an apple.
+
+For each tick a new position for the snake head is calculated by its direction and It is represented by the length of the snake.
+That logic allows us to move the snake by subtracting 1 to all values between 1 and 127.
+
--- main.cpp
+++ main.cpp
@@ -14,22 +14,12 @@ using namespace std;
 const int WIDTH = 20;
 const int HEIGHT = 20;
 
-typedef char Matrix [WIDTH][HEIGHT];
-
 struct vec2{
 	int x;
 	int y;
 };
 
-struct GameState{
-	bool is_running = false;
-	//Default value 16tps
-	int ticks_per_second = 16;
-
-	/*
-		La culebra
-	*/
-};
+typedef char Matrix [WIDTH][HEIGHT];
 
 void initMatrix(Matrix &m, char a){
 	int i, j;
@@ -49,25 +39,124 @@ void showMatrix(Matrix m){
 	for (i=0; i < HEIGHT; i++){
 		//j 0..19
 		for (j=0; j < WIDTH; j++){
-			cout << m[i][j] << ' ';
+			cout << (int)m[i][j] << ' ';
 		}
 		cout<<endl;
 
 	}
 }
 
-void tick(GameState &game_state){
+//POST={The method substracts 1 for all natural numbers on the table >= 1}
+void matrixNaturalsMinusOne(Matrix & m){
+	int i, j;
+
+	for (i=0; i < HEIGHT; i++){
+		for (j=0; j < WIDTH; j++){
+			if(m[i][j] >= 1){
+				m[i][j] -= 1;
+			}
+		}
+	}
+}
+
+enum SnakeDirection{
+	UP		=0x01, 
+	DOWN	=0x02, 
+	LEFT	=0x04, 
+	RIGHT =0x08
+};
+
+struct GameState{
+	bool is_running = false;
+	//Default value 16tps
+	int ticks_per_second = 16;
+
+	Matrix table;
 	
+	/*
+		La culebra
+	*/
+	vec2 head_position = vec2{};
+	SnakeDirection snake_direction = DOWN;
+	char snake_length = 1;
+};
+
+
+/**
+ * ===============
+ * Snake functions
+ * ===============
+ */
+void moveSnake(GameState &game_state){
+
+	matrixNaturalsMinusOne(game_state.table);
+	
+	//Generate the head in the next position
+	vec2 new_head_position = game_state.head_position;
+
+	//TODO Boundary check
+	switch(game_state.snake_direction){
+		case UP:
+			new_head_position.y--;
+			break;
+		case DOWN:
+			new_head_position.y++;
+			break;
+		case LEFT:
+			new_head_position.x--;
+			break;
+		case RIGHT:
+			new_head_position.x++;
+			break;
+	}
+
+	if(new_head_position.x < 0){
+		new_head_position.x = WIDTH-1;
+	}
+	if(new_head_position.x >= WIDTH){
+		new_head_position.x = 0;
+	}
+	if(new_head_position.y < 0){
+		new_head_position.y = HEIGHT-1;
+	}
+	if(new_head_position.y >= HEIGHT){
+		new_head_position.y = 0;
+	}
+
+	game_state.head_position = new_head_position;
+
+	game_state.table[game_state.head_position.x][game_state.head_position.y] = game_state.snake_length;
+
+}
+
+/**
+ * ====================
+ * Game Logic Functions
+ * ====================
+ */
+
+void tick(GameState &game_state){
+	moveSnake(game_state);
+
+	showMatrix(game_state.table);
+	cout << endl;
 }
 
 int main(){
 
 	GameState game_state;
-	game_state.is_running= true;
+	game_state.is_running = true;
+	game_state.ticks_per_second = 3;
 
-	Matrix table;
-	initMatrix(table, '0');
-	showMatrix(table);
+	game_state.snake_length = 4;
+
+	initMatrix(game_state.table, 0);
+
+	//Generate the snake in the center of the table
+	game_state.head_position = vec2{
+		.x = WIDTH/2,
+		.y = HEIGHT/2
+	};
 
 	while(game_state.is_running){
 
