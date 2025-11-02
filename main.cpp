71f08cee750ea364219ffe64351f2e7049d6ddd1
elarle <arbol@git.ak>
2025-11-02 22:48:54 +0100

    Added payoui as IO handler. Now the snake has color and movement

--- main.cpp
+++ main.cpp
@@ -8,10 +8,11 @@
 
 #include <iostream>
 #include <thread>
+#include "payoui/lib/payoui.hpp"
 
 using namespace std;
 
-const int WIDTH = 20;
+const int WIDTH = 40;
 const int HEIGHT = 20;
 
 struct vec2{
@@ -36,9 +37,9 @@ void showMatrix(Matrix m){
 	int i, j;
 
 	//i 0..19
-	for (i=0; i < HEIGHT; i++){
+	for (i=0; i < WIDTH; i++){
 		//j 0..19
-		for (j=0; j < WIDTH; j++){
+		for (j=0; j < HEIGHT; j++){
 			cout << (int)m[i][j] << ' ';
 		}
 		cout<<endl;
@@ -50,8 +51,8 @@ void showMatrix(Matrix m){
 void matrixNaturalsMinusOne(Matrix & m){
 	int i, j;
 
-	for (i=0; i < HEIGHT; i++){
-		for (j=0; j < WIDTH; j++){
+	for (i=0; i < WIDTH; i++){
+		for (j=0; j < HEIGHT; j++){
 			if(m[i][j] >= 1){
 				m[i][j] -= 1;
 			}
@@ -67,11 +68,14 @@ enum SnakeDirection{
 };
 
 struct GameState{
+
 	bool is_running = false;
 	//Default value 16tps
 	int ticks_per_second = 16;
 
 	Matrix table;
+	//This is needed for faster game drawing
+	Matrix old_table;
 	
 	/*
 		La culebra
@@ -79,6 +83,8 @@ struct GameState{
 	vec2 head_position = vec2{};
 	SnakeDirection snake_direction = DOWN;
 	char snake_length = 1;
+
+	PUI::Terminal terminal;
 };
 
 
@@ -135,22 +141,85 @@ void moveSnake(GameState &game_state){
  * ====================
  */
 
+void updateScreen(GameState & game_state){
+	int i, j;
+
+	for (i=0; i < WIDTH; i++){
+		for (j=0; j < HEIGHT; j++){
+			if(game_state.table[i][j] != game_state.old_table[i][j]){
+				
+				game_state.terminal.moveCursor(i, j);
+				
+				if(game_state.table[i][j] == 0){
+					game_state.terminal.setForegroundRGB(64, 64, 64);
+					game_state.terminal.writeText("·");
+				} else { // :(
+					game_state.terminal.setForegroundRGB(
+						255 - ((float)game_state.table[i][j] / game_state.snake_length)*255,
+						255, 
+						255 - ((float)game_state.table[i][j] / game_state.snake_length)*255
+					);
+					game_state.terminal.writeText(
+						game_state.table[i][j] > 0 ? "O" : "*"
+					);
+				}
+				game_state.old_table[i][j] = game_state.table[i][j];
+			}
+		}
+	}
+}
+
 void tick(GameState &game_state){
 	moveSnake(game_state);
 
-	showMatrix(game_state.table);
+	updateScreen(game_state);
 	cout << endl;
+}
+
+GameState * _games_state = nullptr;
+
+void keyHandler(char input){
+	if(_games_state != nullptr){
+		switch(input){
+			case 'w':
+				_games_state->snake_direction = UP;
+				break;
+			case 's':
+				_games_state->snake_direction = DOWN;
+				break;
+			case 'a':
+				_games_state->snake_direction = LEFT;
+				break;
+			case 'd':
+				_games_state->snake_direction = RIGHT;
+				break;
+			case 'q':
+				_games_state->is_running = false;
+				break;
+		}
+	}
+
 }
 
 int main(){
 
 	GameState game_state;
+	_games_state = &game_state;
+
+	//Important to init the terminal first
+	game_state.terminal.init();
+	game_state.terminal.onKeyPress = keyHandler;
+	game_state.terminal.clear();
+	game_state.terminal.hideCursor();
+
 	game_state.is_running = true;
 	game_state.ticks_per_second = 3;
 
-	game_state.snake_length = 4;
+	game_state.snake_length = 16;
 
 	initMatrix(game_state.table, 0);
+	//We init it with a diferent value to draw the entire table in the first tick
+	initMatrix(game_state.old_table, -1);
 
 	//Generate the snake in the center of the table
 	game_state.head_position = vec2{
@@ -164,6 +233,9 @@ int main(){
 		this_thread::sleep_for(chrono::milliseconds(1000/game_state.ticks_per_second));
 
 	}
+
+	game_state.terminal.hideCursor();
+	game_state.terminal.close();
 
 	return 0;
 }
